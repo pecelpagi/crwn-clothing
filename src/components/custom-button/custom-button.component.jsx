670840dd03cd24b2c44f5cb686046c001ffc1332
Galuh Ramadhan <galuh.html@gmail.com>
2021-12-27 06:37:34 +0700

    sign in page

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({ children, ...rest }) => (
+    <button className="custom-button" {...rest}>{children}</button>
+)
+
+export default CustomButton;
